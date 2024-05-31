612f5790366064640a9a45854e9adfc5844f3e1e
shubdixit <shubhashish.dixit@beyondirr.tech>
2024-05-31 12:35:35 +0530

    Modify Allowed Hosts

--- bitespeed/settings.py
+++ bitespeed/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEVELOPMENT").lower() == "true"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
