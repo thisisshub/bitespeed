bafa97a4620ac24c5861226fd6440851f4126d31
Shubhashish Dixit <thisisshub@gmail.com>
2024-06-07 13:33:55 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("identify/", views.IdentifyViewSet.as_view({"post": "create"}), name="identify"),
+    path("", views.IdentifyViewSet.as_view({"post": "create"}), name="identify"),
 ]
